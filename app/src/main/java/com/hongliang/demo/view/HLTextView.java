6793e0836e973d29a1c0bf1cbc0fb0b03ed4ae8b
yuqian <634229805@qq.com>
2019-07-08 21:54:31 +0800

    View与Activity生命周期

--- app/src/main/java/com/hongliang/demo/view/HLTextView.java
+++ app/src/main/java/com/hongliang/demo/view/HLTextView.java
@@ -3,6 +3,7 @@ package com.hongliang.demo.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -14,8 +15,69 @@ import com.hongliang.demo.R;
 public class HLTextView extends TextView {
 
 
+    @Override
+    protected void onFinishInflate() {
+        log("View onFinishInflate 子控件均被映射成xml");
+        super.onFinishInflate();
+
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        log("View onAttachedToWindow 当前View附加到Window");
+        super.onAttachedToWindow();
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        log("View onMeasure View测量");
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        log("View onSizeChanged View尺寸发生变化");
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        log("View onLayout 设置View的位置");
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        log("View onWindowFocusChanged View所在Window获得或失去焦点");
+        super.onWindowFocusChanged(hasWindowFocus);
+    }
+
+
+    @Override
+    protected void onWindowVisibilityChanged(int visibility) {
+        log("View onWindowVisibilityChanged View或其祖先的可见性改变");
+        super.onWindowVisibilityChanged(visibility);
+
+    }
+
+
+    @Override
+    protected void onDetachedFromWindow() {
+        log("View onDetachedFromWindow View从Window上分离");
+        super.onDetachedFromWindow();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        log("View onDraw Canvas上开始绘制View");
+        super.onDraw(canvas);
+    }
+
     public HLTextView(Context context) {
-        super(context);
+        this(context,null);
     }
 
     public HLTextView(Context context, @Nullable AttributeSet attrs) {
@@ -25,6 +87,7 @@ public class HLTextView extends TextView {
     public HLTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         parse(context, attrs, defStyleAttr);
+        log("View TextView 调用构造方法");
     }
 
     private void parse(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -34,15 +97,10 @@ public class HLTextView extends TextView {
         String three = typedArray.getString(R.styleable.ZTextViewStyle_attr_3);
         String four = typedArray.getString(R.styleable.ZTextViewStyle_attr_4);
 
-        log("one = " + one);
-        log("two = " + two);
-        log("three = " + three);
-        log("four = " + four);
-
         typedArray.recycle();
     }
 
     private void log(String msg) {
-        Log.i("ZDL", msg);
+        Log.i("TAG", msg);
     }
 }
--- app/src/main/java/com/hongliang/demo/viewActivity/AttributeSetActivity.java
+++ app/src/main/java/com/hongliang/demo/viewActivity/AttributeSetActivity.java
@@ -2,7 +2,9 @@ package com.hongliang.demo.viewActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.hongliang.demo.R;
 
@@ -12,8 +14,66 @@ public class AttributeSetActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        log("Activity onCreate");
         setContentView(R.layout.activity_view_attributset);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        log("Activity onPostCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        log("Activity onStart");
+    }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        log("Activity onResume");
+    }
+
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        log("Activity onPostResume");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        log("Activity onStop");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        log("Activity onPause");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        log("Activity onDestroy");
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        log("Activity onSaveInstanceState");
+    }
+
+    private void log(String msg) {
+        Log.i("TAG", msg);
     }
 }
