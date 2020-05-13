6ff06b6bb6e1d8eb4bd68a41e48939508928abd7
yuqian <634229805@qq.com>
2020-05-13 22:34:37 +0800

    View 滑动

--- app/src/main/java/com/hongliang/demo/otherActivity/GlideActivity.java
+++ app/src/main/java/com/hongliang/demo/otherActivity/GlideActivity.java
@@ -2,10 +2,13 @@ package com.hongliang.demo.otherActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.support.v4.text.PrecomputedTextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,15 +16,21 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.hongliang.demo.R;
+import com.hongliang.demo.util.BitmapUtil;
 
+import java.io.File;
+
+import me.zhouzhuo810.cameracardcrop.BitmapUtils;
 import me.zhouzhuo810.cameracardcrop.CameraConfig;
 import me.zhouzhuo810.cameracardcrop.CropActivity;
 
 public class GlideActivity extends Activity {
 
     private ImageView mIvGlide;
-    private String url = "https://cn.bing.com/th?id=OHR.GobiSheep_EN-CN3893154532_1920x1080.jpg";
+    private String url = "http://pic.netbian.com/uploads/allimg/180315/110404-1521083044b19d.jpg";
 
+    String dir_JPG = Environment.getExternalStorageDirectory().getAbsolutePath() + "/image.JPG";
+    String dir_PNG = Environment.getExternalStorageDirectory().getAbsolutePath() + "/p_image.PNG";
     private ImageView iv_image;
 
     @Override
@@ -29,35 +38,8 @@ public class GlideActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glide);
         initView();
-        String dir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tencent/MicroMsg/WeiXin/";
-
-
-        Log.i("LOG", "=======" + dir);
-
-        Glide.with(this)
-                .load(url)
-                .placeholder(R.drawable.avastar)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .error(R.mipmap.ic_launcher)
-                .into(mIvGlide);
-
-        //  GlideApp.with(this).load(url).into(mIvGlide);
-
-      /*  Glide.with(this).load(url).listener(new RequestListener<Drawable>() {
-            @Override
-            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                Log.i("LOG","===============onLoadFailed");
-                return false;
-
-            }
-
-            @Override
-            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                Log.i("LOG","=================onResourceReady");
-                return false;
-            }
-        }).into(mIvGlide);*/
-
+        String dir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/image.JPG";
+        Log.i("LOG", "dir" + dir);
 
         findViewById(R.id.bt_touch).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +56,11 @@ public class GlideActivity extends Activity {
                 startActivityForResult(intent, 0x01);
             }
         });
+
+
+        MyTask mTask = new MyTask();
+        mTask.execute();
+
 
     }
 
@@ -94,4 +81,71 @@ public class GlideActivity extends Activity {
             }
         }
     }
+
+
+    private class MyTask extends AsyncTask<String, Integer, Bitmap> {
+
+
+        // 方法1：onPreExecute（）
+        // 作用：执行 线程任务前的操作
+        @Override
+        protected void onPreExecute() {
+            //   text.setText("加载中");
+            // 执行前显示提示
+        }
+
+
+        // 方法2：doInBackground（）
+        // 作用：接收输入参数、执行任务中的耗时操作、返回 线程任务执行的结果
+        // 此处通过计算从而模拟“加载进度”的情况
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            return BitmapUtil.getBitmapByUrl(url);
+        }
+
+        // 方法3：onProgressUpdate（）
+        // 作用：在主线程 显示线程任务执行的进度
+        @Override
+        protected void onProgressUpdate(Integer... progresses) {
+            //     progressBar.setProgress(progresses[0]);
+            //    text.setText("loading..." + progresses[0] + "%");
+        }
+
+        // 方法4：onPostExecute（）
+        // 作用：接收线程任务执行结果、将执行结果显示到UI组件
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            // 执行完毕后，则更新UI
+            //  text.setText("加载完毕");
+
+            ;
+            ;
+
+            Log.i("LOG", "========JPG设置2000*1000分辨率后内存" + BitmapUtil.getBitmapSize(BitmapUtil.bitmapFactory(dir_JPG, 2000, 1000)));//8294400
+            Log.i("LOG", "========JPG设置1000*500分辨率后内存" + BitmapUtil.getBitmapSize(BitmapUtil.bitmapFactory(dir_JPG, 1000, 500)));//2073600
+
+            Log.i("LOG", "========PNG设置2000*1000分辨率后内存" + BitmapUtil.getBitmapSize(BitmapUtil.bitmapFactory(dir_PNG, 2000, 1000)));//17766672
+            Log.i("LOG", "========PNG设置1000*500分辨率后内存" + BitmapUtil.getBitmapSize(BitmapUtil.bitmapFactory(dir_PNG, 1000, 500)));// 4439160
+
+
+            BitmapUtil.saveImage(BitmapUtil.bitmapFactory(dir_JPG, 2000, 1000), Bitmap.CompressFormat.JPEG, 100, "j_image", ".JPG");//1920*1080 1M
+            BitmapUtil.saveImage(BitmapUtil.bitmapFactory(dir_JPG, 1000, 500), Bitmap.CompressFormat.JPEG, 100, "j2_image", ".JPG");//960*540 253.4kb
+
+            BitmapUtil.saveImage(BitmapUtil.bitmapFactory(dir_PNG, 2000, 1000), Bitmap.CompressFormat.JPEG, 100, "p_image", ".PNG");//2508*1771 361.7kb
+            BitmapUtil.saveImage(BitmapUtil.bitmapFactory(dir_PNG, 1000, 500), Bitmap.CompressFormat.JPEG, 100, "p2_image", ".PNG");//1254*885 131kb
+
+
+            iv_image.setImageBitmap(BitmapUtil.getBitmapByPhone(dir_JPG));
+        }
+
+        // 方法5：onCancelled()
+        // 作用：将异步任务设置为：取消状态
+        @Override
+        protected void onCancelled() {
+            //   text.setText("已取消");
+            //   progressBar.setProgress(0);
+
+        }
+    }
+
 }
---
+++ app/src/main/java/com/hongliang/demo/util/BitmapUtil.java
@@ -0,0 +1,166 @@
+package com.hongliang.demo.util;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BitmapUtil {
+
+    /**
+     * 加载网络图片
+     *
+     * @param url
+     * @return
+     */
+    public static Bitmap getBitmapByUrl(String url) {
+        URL imgUrl = null;
+        Bitmap bitmap = null;
+        try {
+            imgUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) imgUrl
+                    .openConnection();
+            conn.setDoInput(true);
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            bitmap = BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+
+    /**
+     * 根据本地图片地址加载图片
+     *
+     * @param url
+     * @return
+     */
+    public static Bitmap getBitmapByPhone(String url) {
+        Bitmap bitmap = BitmapFactory.decodeFile(url);
+
+        return bitmap;
+    }
+
+
+    public static Bitmap getBitmapByPhoneCompress(String url, Bitmap.CompressFormat format, int quality) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Bitmap bitmap = BitmapFactory.decodeFile(url);
+        bitmap.compress(format, quality, baos);
+        ByteArrayInputStream isBm = new ByteArrayInputStream(baos.toByteArray());
+        bitmap = BitmapFactory.decodeStream(isBm, null, null);// 把ByteArrayInputStream数据生成图片
+        return bitmap;
+    }
+
+    /**
+     * 计算图片大小 适用于网络，本地等图片
+     * 内存大小 = 图片长度 x 图片宽度 x 单位像素占用的字节数
+     *
+     * @param bitmap
+     * @return
+     */
+    public static int getBitmapSize(Bitmap bitmap) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {    //API 19
+            return bitmap.getAllocationByteCount();
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {//API 12
+            return bitmap.getByteCount();
+        }
+        // 在低版本中用一行的字节x高度
+        return bitmap.getRowBytes() * bitmap.getHeight();                //earlier version
+    }
+
+
+    public static Bitmap getBitmap565(String url) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+        Bitmap bitmap = BitmapFactory.decodeFile(url, options);
+        return bitmap;
+    }
+
+
+    /**
+     * 根据图片路径进行压缩保存图片
+     *
+     * @param bitmap
+     * @param format
+     * @param quality
+     * @param name
+     * @param type
+     */
+    public static void saveImage(Bitmap bitmap, Bitmap.CompressFormat format, int quality, String name, String type) {
+        String dir = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File dirFile = new File(dir);
+        if (!dirFile.exists()) {
+            dirFile.mkdirs();
+        }
+
+        File file = new File(dir, name + quality + type);
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            bitmap.compress(format, quality, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Bitmap bitmapFactory(String url, int width, int height) {
+
+        // 配置压缩的参数
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true; //获取当前图片的边界大小，而不是将整张图片载入在内存中，避免内存溢出
+        BitmapFactory.decodeFile(url, options);
+        options.inJustDecodeBounds = false;
+        ////inSampleSize的作用就是可以把图片的长短缩小inSampleSize倍，所占内存缩小inSampleSize的平方
+        options.inSampleSize = caculateSampleSize(options, width, height);
+        Bitmap bm = BitmapFactory.decodeFile(url, options); // 解码文件
+        return bm;
+    }
+
+    /**
+     * 计算出所需要压缩的大小
+     *
+     * @param options
+     * @param reqWidth  我们期望的图片的宽，单位px
+     * @param reqHeight 我们期望的图片的高，单位px
+     * @return
+     */
+    private static int caculateSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        int sampleSize = 1;
+        int picWidth = options.outWidth;
+        int picHeight = options.outHeight;
+        if (picWidth > reqWidth || picHeight > reqHeight) {
+            int halfPicWidth = picWidth / 2;
+            int halfPicHeight = picHeight / 2;
+            while (halfPicWidth / sampleSize > reqWidth || halfPicHeight / sampleSize > reqHeight) {
+                sampleSize *= 2;
+            }
+        }
+        return sampleSize;
+    }
+
+
+}
