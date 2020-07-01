b0d827115c67d92458966c11e612066983ca2dbe
yuqian <634229805@qq.com>
2020-07-02 00:48:15 +0800

    提交

--- app/src/main/java/com/hongliang/demo/view/PaintView.java
+++ app/src/main/java/com/hongliang/demo/view/PaintView.java
@@ -16,12 +16,16 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+import com.hongliang.demo.R;
 import com.hongliang.demo.util.UIHelper;
 
 
 public class PaintView extends View {
-    private Context mContext;
 
+
+    private Paint mPaint;
+    private Context mContext;
+    int textSize;
     public PaintView(Context context) {
         this(context, null);
     }
@@ -34,6 +38,29 @@ public class PaintView extends View {
     public PaintView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mContext = context;
+
+         textSize = UIHelper.sp2px(mContext, 14);
+        TextPaint fontPaint = new TextPaint();
+        fontPaint.setTextSize(textSize);
+
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        mPaint.setStrokeWidth(5);
+        mPaint.setTextSize(textSize);
+        mPaint.setTextAlign(Paint.Align.LEFT);
+        mPaint.setStrikeThruText(true);
+        mPaint.setTypeface(Typeface.DEFAULT_BOLD);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        float sizeWidth = mPaint.measureText("100") + 100;
+
+        setMeasuredDimension((int) sizeWidth, 50);
     }
 
 
@@ -42,24 +69,14 @@ public class PaintView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        int left = 200;
-        int textSize = UIHelper.Dp2Px(mContext, 14);
-        TextPaint FontPaint = new TextPaint();
-        FontPaint.setTextSize(textSize);
+        int left = 0;
 
-        String text = "我爱中国";
 
-        Paint mPaint = new Paint();
-        mPaint.setColor(Color.BLUE);
-        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        mPaint.setStrokeWidth(5);
-        mPaint.setTextSize(textSize);
-        mPaint.setTextAlign(Paint.Align.LEFT);
+        String text = "100";
 
-        mPaint.setStrikeThruText(true);
-        mPaint.setTypeface(Typeface.DEFAULT_BOLD);
-        canvas.drawText(text, left, 100, mPaint);
 
+        canvas.drawLine(0, 25, 100, 25, mPaint);
+         canvas.drawText(text, 100, 25, mPaint);
         Log.i("bqt", "ascent()的值为" + mPaint.ascent() + "，descent()的值为" + mPaint.descent());//
         Log.i("bqt", "字符的高度为" + (-mPaint.ascent() + mPaint.descent()) + "，字符的长度为" + mPaint.measureText(text) + "字符的长度为" + UIHelper.getTextWidth(text, textSize));//4
         //   Log.i("bqt", "测量的高度为" + tv_info.getMeasuredHeight() + "，测量的宽度为" + tv_info.getMeasuredWidth());//49，190
@@ -67,12 +84,25 @@ public class PaintView extends View {
         mPaint.getTextBounds(text, 0, text.length(), mRect);
         Log.i("bqt", "字符的边界为" + mRect.left + "，" + mRect.top + "，" + mRect.right + "，" + mRect.bottom);//
         Log.i("bqt", "字符的高度为" + (mRect.bottom - mRect.top) + "，字符的长度为" + (mRect.right - mRect.left));//
-
-
     }
 
-    private void log(String message) {
-        Log.i("TAG", message);
-    }
 
+    public static int getDefaultSize(int size, int measureSpec) {
+        int result = size;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        switch (specMode) {
+            case MeasureSpec.UNSPECIFIED: //尽可能的多，一般是系统方法
+                result = size;
+                break;
+            case MeasureSpec.AT_MOST://wrap_content
+
+                break;
+            case MeasureSpec.EXACTLY://精确的值或者为match_paren
+                result = specSize;
+                break;
+        }
+        return result;
+    }
 }
